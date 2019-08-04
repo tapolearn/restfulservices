55232d59be779a1b1a7b226bc5469667b921952d
Raghunatha Tadipatri <tadipatri.raghunatha@gmail.com>
2019-08-04 19:46:35 +0530

    Updated swagger documentation.

--- src/main/java/com/taposoft/learning/springboot/restfulservices/entity/User.java
+++ src/main/java/com/taposoft/learning/springboot/restfulservices/entity/User.java
@@ -6,9 +6,12 @@ import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Entity
+@ApiModel(description = "This is the user details class.")
 public @Data class User {
 	
 	@Id
@@ -16,10 +19,13 @@ public @Data class User {
 	private Integer id;
 	
 	@Size(min = 2)
+	@ApiModelProperty(notes = "Should be of minimum 2 characters.")
 	private String firstName;
 	@Size(min = 2)
+	@ApiModelProperty(notes = "Should be of minimum 2 characters.")
 	private String lastName;
 	@Email
+	@ApiModelProperty(notes = "Should be a proper email format.")
 	private String email;
 	private String phoneNumber;
 }
